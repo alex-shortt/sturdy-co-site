c202317081e86d7a2f5bed244ec73dfb88aaa761
Alex Shortt <alexander.shortt@gmail.com>
2020-01-25 18:11:18 -0800

    add slices w/ animation logic

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components/macro"
 
+import Slice from "./components/Slice"
+
 const Container = styled.div`
   background: red;
+  border: 1px solid green;
   display: flex;
   justify-content: center;
   align-items: center;
-`
-
-const InteractionSlice = styled.div`
-  flex: 1;
-  height: 100%;
-  cursor: pointer;
-  transition: 0.25s linear;
-  ${props => `background: hsl(0, 0%, ${props.pos * 100}%)`};
+  position: relative;
 `
 
 export default function Carousel(props) {
   const { data } = props
 
   const [dims, setDims] = useState(null)
+  const [activeIndex, setActiveIndex] = useState(null)
 
   useEffect(() => {
     if (!dims) {
@@ -29,16 +26,24 @@ export default function Carousel(props) {
   }, [dims])
 
   return (
-    <Container style={dims}>
+    <Container
+      style={dims}
+      onClick={() => setActiveIndex(Math.floor(Math.random() * data.length))}
+    >
       {data.map((item, i) => (
-        <InteractionSlice id={item.name} pos={i / data.length} />
+        <Slice
+          item={{ ...item, i }}
+          parentDims={dims || getSlicesDims()}
+          data={data}
+          activeIndex={activeIndex}
+        />
       ))}
     </Container>
   )
 }
 
 const getSlicesDims = () => {
-  const ratio = 9 / 16
+  const ratio = 7 / 16
   const width = Math.min(window.innerWidth * 0.7, 900)
   return { width, height: width * ratio }
 }
