bddceae5e5115ca0e6990831ce8c72d777700768
Alex Shortt <alexander.shortt@gmail.com>
2020-02-02 20:04:21 -0800

    add correct logo and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,67 @@
+import React from "react"
+import styled, { css } from "styled-components/macro"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link as LinkBase } from "react-router-dom"
+
+const Container = styled.div`
+  width: 100%;
+  text-align: center;
+`
+
+const Tag = styled.h5`
+  line-height: 1.5em;
+`
+
+const Info = styled.h4`
+  color: #2c2c2c;
+`
+
+const IG = styled(FontAwesomeIcon).attrs({ icon: ["fab", "instagram"] })`
+  font-size: 1.4em;
+  vertical-align: text-bottom;
+`
+
+const Spacer = styled.span`
+  margin: 0 15px;
+`
+
+const linkStyles = css`
+  cursor: pointer;
+  color: inherit !important;
+  transition: 150ms linear;
+  text-decoration: none;
+
+  &:hover {
+    color: white !important;
+  }
+`
+const Anchor = styled.a`
+  ${linkStyles}
+`
+
+const Link = styled(LinkBase)`
+  ${linkStyles}
+`
+
+export default function Footer(props) {
+  const { ...restProps } = props
+
+  return (
+    <Container {...restProps}>
+      <Tag>
+        CREATIVE HOUSE & DIGITAL DELVE
+        <br />
+        EST. 2018
+      </Tag>
+      <Info>
+        <Anchor href="https://instagram.com/sturdyco" about="_blank">
+          <IG />
+        </Anchor>
+        <Spacer>|</Spacer>
+        <Link to="/privacy">PRIVACY POLICY</Link>
+        <Spacer>|</Spacer>
+        &copy; STURDY 2020 - ALL RIGHTS RESERVED
+      </Info>
+    </Container>
+  )
+}
--- src/scenes/View/index.js
+++ src/scenes/View/index.js
@@ -3,7 +3,9 @@ import styled from "styled-components/macro"
 
 import Helmet from "components/Helmet"
 import Carousel from "components/Carousel"
+import FooterBase from "components/Footer"
 import clients from "assets/data/clients"
+import sturdyImg from "assets/images/logo.png"
 
 const Container = styled.div`
   height: 100%;
@@ -14,11 +16,27 @@ const Container = styled.div`
   align-items: center;
 `
 
+const Logo = styled.img.attrs({ src: sturdyImg })`
+  height: calc(70px + 4vw);
+  width: 100%;
+  object-fit: contain;
+  position: absolute;
+  top: calc(26% - 6.2vw);
+  transform: translateY(-50%);
+`
+
+const Footer = styled(FooterBase)`
+  position: absolute;
+  bottom: 0;
+`
+
 export default function View(props) {
   return (
     <Container>
       <Helmet title="Home" />
+      <Logo />
       <Carousel data={clients} />
+      <Footer />
     </Container>
   )
 }
